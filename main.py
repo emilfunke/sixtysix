4f9b43e30c4ac24349aebb5985fecab809e18972
emilfunke <emil.funke98@gmail.com>
2022-03-02 00:00:42 +0100

    beginning to add stab rules

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ class Player():
         self.hand = []
         self.count = 0
 
-
+class Card():
+    def __init__(self, value, color):
+        self.value = value
+        self.color = color
 """
 create the 52 card deck and shuffle it
 return: shuffled deck -> list
@@ -26,7 +29,7 @@ def build_Deck():
 
     for c in color:
         for v in value:
-            card = "{}, {}".format(c, v)
+            card = Card(v, c)
             deck.append(card)
     shuffle(deck)
     return deck
@@ -38,13 +41,26 @@ def draw_card(deck):
 
 def determine_trump(deck):
     trump_card = deck.pop()
-    trump_color = ""
-    for i in range(len(trump_card)):
-        if trump_card[i] != ",":
-            trump_color += trump_card[i]
-        else:
-            break
-    return trump_color, trump_card
+    return trump_card.value, trump_card.color
+
+def stab(card_p1, card_p2, trump_color):
+    count_p1 = 0
+    count_p2 = 0
+
+    v_p1 = card_p1.value
+    v_p2 = card_p2.value
+    c_p1 = card_p1.color
+    c_p2 = card_p2.color
+
+
+    if (c_p2 != trump_color & c_p1 != trump_color):
+        if v_p1 > v_p2:
+            return v_p1+v_p2, 0
+        elif v_p2 > v_p1:
+            return 0, v_p1+v_p2
+
+
+    return count_p1, count_p2
 
 def game(deck):
     p1 = Player(input("Give P1 a name: "))
@@ -57,9 +73,13 @@ def game(deck):
     trump_color, trump_card = determine_trump(deck_66)
 
     print(p1.hand)
+    print(p2.hand)
+
 
     print(trump_color)
     print(trump_card)
+
+    stab(p1.hand.pop(), p2.hand.pop(), trump_color)
 
 
 deck_66 = build_Deck()
